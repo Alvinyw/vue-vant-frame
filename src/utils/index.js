0e04a3413274e8b8a591c6382f918f77ebf53d93
Alvinyw <alvin.yiwen@gmail.com>
2020-02-27 17:09:30 +0800

    去除不常用的文件

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import * as session from './session'
 import * as cookies from './cookies'
 import fieldValidater from './field-validator'
-import asyncValidater from './form-validator'
 import * as lib from './lib' // 全局工具方法
 
 Vue.prototype.$session = session // session 操作全局方法
 Vue.prototype.$cookies = cookies // cookie 操作全局方法
 Vue.prototype.$fieldValidater = fieldValidater // 表单字段字段校验全局方法
-Vue.prototype.$validate = asyncValidater // 对第三方验证框架 async-validator 的封装
 Vue.prototype.$lib = lib
--- vue.config.js
+++ vue.config.js
@@ -18,11 +18,8 @@ module.exports = {
     proxy: {
       '/api': {
         // 此处并非和url一致
-        // target: 'http://192.168.135.229:8087', // 客户明细
-        // target: 'http://192.168.135.229:8087', // 永强
-        target: 'http://192.168.135.229:8082/wallet', // 永强
-        // target: 'http://192.168.135.193:8082/wallet', // 伟杰
-        // target: 'http://192.168.135.193:8087', // 伟杰
+        target: 'http://192.168.135.229:8082/wallet', // 测试
+        // target: 'http://192.168.135.193:8082/wallet', // 本地
         changeOrigin: true, // 允许跨域
         ws: true,
         pathRewrite: {
